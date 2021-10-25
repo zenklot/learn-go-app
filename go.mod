b3e65c5e3a55cdc2c7f5eb404ce1b2cba9ad8b0c
zenklot <zenklot11@gmail.com>
2021-10-25 21:33:12 +0700

    writing code with module dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zenklot/learn-go-app
+
+go 1.17
+
+require github.com/zenklot/learn-go-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	learn_go_module "github.com/zenklot/learn-go-module"
+)
+
+func main() {
+	fmt.Println(learn_go_module.SayHello())
+}
